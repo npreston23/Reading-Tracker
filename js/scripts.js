01699b2784ab2332f77b46c5ad52dbbe5ae6709a
Guest User <Guest@Epicodus-8B.local>
2018-07-18 19:11:16 -0700

    js added with click functions for menu items of navbar

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,23 @@ $(document).ready(function() {
     $("#currentBook").show();
     $(".row").hide();
     $(".wrapper").hide();
-  });  
+  });
+
+  $("#toRead").click(function(event) {
+    event.preventDefault();
+    $("#currentBook").hide();
+    $(".row").hide();
+    $(".wrapper").hide();
+    $(".add-to-read").show();
+  });
+
+  $("#booksRead").click(function(event) {
+    event.preventDefault();
+    $("#currentBook").hide();
+    $(".row").hide();
+    $(".wrapper").hide();
+    $(".add-to-read").hide();
+    $(".books-read").show();
+  });
+
 });
